4c805c663b8b887067a96ce8e5292e5c15707c04
shane325 <shane325@gmail.com>
2018-06-20 14:35:28 -0700

    Made some UI changes to the table. Added img and url for each row.

--- public/client.js
+++ public/client.js
@@ -1,17 +1,16 @@
-CudaNewsApp = angular.module('CudaNewsApp', ['CudaNewsApp.controllers', 'smart-table']);
- 
-angular.module('CudaNewsApp.controllers', []).controller('CudaNewsController',  ['$scope', '$http', function($scope, $http) {
- $scope.loading = false;
- $scope.getData = function() {
-    $scope.loading = true;
+CudaNewsApp = angular.module('CudaNewsApp', ['CudaNewsApp.controllers', 'smart-table'])
+
+angular.module('CudaNewsApp.controllers', []).controller('CudaNewsController', ['$scope', '$http', function ($scope, $http) {
+  $scope.loading = false
+  $scope.getData = function () {
+    $scope.loading = true
     $http.get('http://localhost:8080/api/articles')
-        .then(function(response){
-             $scope.news = response.data;
-             console.log(response.data);
-             $scope.loading = false;
-     });
- }
+      .then(function (response) {
+        $scope.news = response.data.articles
+        console.log(response.data)
+        $scope.loading = false
+      })
+  }
 
- $scope.getData();
-
-}]);
+  $scope.getData()
+}])
