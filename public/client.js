da7375e4a58dbf5e2c969f94367ff3591a07bcdf
shane325 <shane325@gmail.com>
2018-06-20 21:07:57 -0700

    Firebase integration. Added more angular services. Some code clean up.

--- public/client.js
+++ public/client.js
@@ -1,19 +1,65 @@
 /**
  * Define the CudaNewsApp module
  */
-CudaNewsApp = angular.module('CudaNewsApp', ['CudaNewsApp.controllers', 'smart-table', '720kb.socialshare'])
+CudaNewsApp = angular
+  .module('CudaNewsApp', [
+    'CudaNewsApp.controllers',
+    'CudaNewsApp.services',
+    'smart-table',
+    'firebase'])
 
-angular.module('CudaNewsApp.controllers', []).controller('CudaNewsController', ['$scope', '$http', function ($scope, $http) {
-  $scope.loading = false
-  $scope.getData = function () {
-    $scope.loading = true
-    $http.get('http://localhost:8080/api/articles')
-      .then(function (response) {
-        $scope.news = response.data.articles
-        console.log(response.data)
-        $scope.loading = false
-      })
+angular
+  .module('CudaNewsApp.controllers', [])
+  .controller('CudaNewsController', CudaNewsController)
+
+/**
+ * CudaNewsController
+ */
+
+CudaNewsController.$inject = ['$scope', 'CudaNewsService']
+
+function CudaNewsController ($scope, CudaNewsService) {
+  $scope.articles = CudaNewsService.getArticles()
+}
+
+angular
+  .module('CudaNewsApp.services', [])
+  .factory('CudaNewsService', CudaNewsService)
+  .factory('FirebaseDataService', FirebaseDataService)
+
+/**
+ * CudaNewsService
+ */
+
+CudaNewsService.$inject = ['$firebaseArray', 'FirebaseDataService']
+
+function CudaNewsService ($firebaseArray, FirebaseDataService) {
+  var service = {
+    getArticles: getArticles
   }
 
-  $scope.getData()
-}])
+  return service
+
+  // Return a list of articles
+  function getArticles () {
+    let articles = $firebaseArray(FirebaseDataService.articles)
+    return articles
+  }
+}
+
+/**
+ * FirebaseDataService
+ */
+
+FirebaseDataService.$inject = []
+
+function FirebaseDataService () {
+  let root = firebase.database().ref().child('cudanews')
+
+  var service = {
+    root: root,
+    articles: root.child('articles')
+  }
+
+  return service
+}
